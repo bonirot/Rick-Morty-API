c27dc90fd9ab2b4b3f45ce119bbd874117e79759
Julia Sanchis <jursanchis@gmail.com>
2024-01-16 12:45:31 +0100

    project without bootstrap

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,7 @@
 "use strict";
 const episodesDiv = document.getElementById("episodesDiv");
 const urlRM = "https://rickandmortyapi.com/api/episode";
-const urlPosts = "http://localhost:3000/posts";
-const urlUsers = "http://localhost:3000/users";
-const urlComments = "http://localhost:3000/comments";
 fetch(urlRM)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((episodes) => console.log(episodes));
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -4,57 +4,30 @@ const episodesDiv = document.getElementById("episodesDiv");
 
 const urlRM = "https://rickandmortyapi.com/api/episode";
 
-const urlPosts = "http://localhost:3000/posts";
-const urlUsers = "http://localhost:3000/users";
-const urlComments = "http://localhost:3000/comments";
-
 fetch(urlRM)
   .then((response) => response.json())
-  .then((data) => console.log(data));
-// data.forEach((element) => {
-//   const htmlPosts = ` <a
-//   href="#"
-//   class="list-group-item d-flex gap-3 py-3"
-//   aria-current="true" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  .then((episodes) => console.log(episodes));
 
-//   <!-- Modal -->
-//   <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-//     <div class="modal-dialog">
-//       <div class="modal-content">
-//         <div class="modal-header">
-//           <h1 class="modal-title fs-5" id="staticBackdropLabel">${post.title}</h1>
-//           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//         </div>
-//         <div class="modal-body">
-//         ${post.body}
-//         </div>
-//         <div class="modal-footer">
-//           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//           <button type="button" class="btn btn-primary">Understood</button>
-//         </div>
+// {
+//   data.forEach((element: any) => {
+//     const htmlPosts = ` <a
+//       href="#"
+//       class="list-group-item py-3 lh-sm bg-rm02"
+//       aria-current="true"
+//     >
+//       <div class="d-flex w-100 align-items-center justify-content-between">
+//         <strong class="mb-1">${element}</strong>
 //       </div>
-//     </div>
-//   </div>
-//   <!-- Modal -->
+//       <div class="col-10 mb-1 small">
+//         Some placeholder content in a paragraph below the heading and date.
+//       </div>
+//     </a>`;
+//     episodesDiv.innerHTML += htmlPosts;
+//     console.log(element);
+//   });
+// });
 
-//   <img
-//   src="src/chat-right-dots.svg"
-//   alt="twbs"
-//   width="32"
-//   height="32"
-//   class="flex-shrink-0"
-//   />
-//   <div class="d-flex gap-2 w-100 justify-content-between">
-//   <div>
-//   <h6 id="titlePost" class="mb-0">${post.title}</h6>
-//   <p class="mb-0 opacity-75">
-//   ${post.body}
-//   </p>
-//   </div>
-//   <small class="opacity-50 text-nowrap">now</small>
-//   </div>
-//   </a>`;
-//   divContainer.innerHTML += htmlPosts;
+//console.log(data.results.episode));
 
 // fetch(urlUsers)
 //   .then((response) => response.json())
