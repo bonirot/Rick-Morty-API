d11d5bd9ed252f28d1ce3f93d5a06e4abd25fbef
Julia Sanchis <jursanchis@gmail.com>
2024-01-18 13:39:42 +0100

    code with async functions

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,66 @@
-"use strict";
-const episodesDiv = document.getElementById("episodesDiv");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const urlRM = "https://rickandmortyapi.com/api/episode";
-fetch(urlRM)
-    .then((response) => response.json())
-    .then((episodes) => console.log(episodes));
+function getTitlesEpisodes() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const titleEpisode = yield fetch(urlRM);
+            const data = yield titleEpisode.json();
+            const episodes = data.results;
+            episodes.forEach((episode) => {
+                const episodeContainer = document.getElementById("listContainer");
+                const liEpisode = document.createElement("li");
+                liEpisode.textContent = episode.name;
+                episodeContainer.appendChild(liEpisode);
+                liEpisode.classList.add("episodeList-item");
+            });
+            return data;
+        }
+        catch (error) {
+            throw new Error("fail");
+        }
+    });
+}
+getTitlesEpisodes().then((dataResults) => {
+    getMoreTitles(dataResults);
+});
+function getMoreTitles(dataResults) {
+    const btnLoadEpisodes = document.getElementById("btnLoad");
+    let isAlreadyLoad = true;
+    btnLoadEpisodes.addEventListener("click", () => {
+        if (isAlreadyLoad) {
+            loadMoreTitles(dataResults);
+            isAlreadyLoad = false;
+        }
+    });
+}
+function loadMoreTitles(dataResults) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            if (dataResults.info.next) {
+                const response = yield fetch(dataResults === null || dataResults === void 0 ? void 0 : dataResults.info.next);
+                const data = yield response.json();
+                const episodes = data.results;
+                episodes.forEach((episode) => {
+                    const episodeContainer = document.getElementById("listContainer");
+                    const liEpisode = document.createElement("li");
+                    liEpisode.textContent = episode.name;
+                    episodeContainer.appendChild(liEpisode);
+                });
+                return data;
+            }
+        }
+        catch (error) {
+            throw new Error("fallo");
+        }
+    });
+}
+export {};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,84 @@
-// RICK AND MORTY
-
-const episodesDiv = document.getElementById("episodesDiv");
+import { Main, Info, Episode } from "./interfaces";
 
 const urlRM = "https://rickandmortyapi.com/api/episode";
 
-fetch(urlRM)
-  .then((response) => response.json())
-  .then((episodes) => console.log(episodes));
+async function getTitlesEpisodes(): Promise<Main> {
+  try {
+    const titleEpisode = await fetch(urlRM);
+    const data: Main = await titleEpisode.json();
+    const episodes: Episode[] = data.results;
+
+    episodes.forEach((episode) => {
+      // console.log(episode);
+      const episodeContainer = document.getElementById(
+        "listContainer"
+      ) as HTMLUListElement;
+      const liEpisode = document.createElement("li");
+      liEpisode.textContent = episode.name;
+      episodeContainer.appendChild(liEpisode);
+      liEpisode.classList.add("episodeList-item");
+      // const h4text = document.createElement('h4');
+      // h4text.classList.add('text');
+    });
+
+    return data;
+  } catch (error) {
+    throw new Error("fail");
+  }
+}
+
+getTitlesEpisodes().then((dataResults) => {
+  getMoreTitles(dataResults);
+});
+
+function getMoreTitles(dataResults: Main): void {
+  const btnLoadEpisodes = document.getElementById(
+    "btnLoad"
+  ) as HTMLButtonElement;
+  let isAlreadyLoad: boolean = true;
+  btnLoadEpisodes.addEventListener("click", () => {
+    if (isAlreadyLoad) {
+      loadMoreTitles(dataResults);
+      isAlreadyLoad = false;
+    }
+  });
+}
+
+async function loadMoreTitles(dataResults: Main) {
+  try {
+    if (dataResults.info.next) {
+      const response = await fetch(dataResults?.info.next);
+      const data: Main = await response.json();
+      const episodes: Episode[] = data.results;
+
+      episodes.forEach((episode) => {
+        const episodeContainer = document.getElementById(
+          "listContainer"
+        ) as HTMLUListElement;
+        const liEpisode = document.createElement("li");
+        liEpisode.textContent = episode.name;
+        episodeContainer.appendChild(liEpisode);
+      });
+      return data;
+    }
+  } catch (error) {
+    throw new Error("fallo");
+  }
+}
+
+// fetch(urlRM)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     const nameEpisode: Episodes[] = data.results;
+//     nameEpisode.forEach((episode: Episodes) => {
+//       console.log(episode.name);
+//       const titleEpisode = episode.name;
+//       episodeContainer.innerHTML += `<li class="episodeList-item">
+//           <h4 class="text">${titleEpisode}</h4>
+//         </li>`;
+//     });
+//   });
 
 // {
 //   data.forEach((element: any) => {
