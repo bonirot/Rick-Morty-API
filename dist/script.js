67d62bed84b4931c46e6363fd747dcd12821edcc
Julia Sanchis <jursanchis@gmail.com>
2024-01-22 09:22:34 +0100

    implemented responsive design

--- dist/script.js
+++ dist/script.js
@@ -8,8 +8,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const urlRickMorty = "https://rickandmortyapi.com/api/episode";
-const urlCharacters = "https://rickandmortyapi.com/api/character";
 const episodeContainer = document.getElementById("listContainer");
+const asideEpisodes = document.getElementById("episodeContainer");
 const btnLoad = document.getElementById("btnLoad");
 printContent(urlRickMorty);
 function printContent(url) {
@@ -18,9 +18,13 @@ function printContent(url) {
         const JSONdata = yield data.json();
         const episodes = JSONdata.results;
         episodes.forEach((episode) => {
-            episodeContainer.insertAdjacentHTML("beforeend", `<li id="episode${episode.episode}" class="episodeList-item"><h4 episodeUrl="${episode.url}" class="text">${episode.episode}: ${episode.name}</h4></li>`);
-            const clickEpisodes = document.getElementById(`episode${episode.episode}`);
+            episodeContainer.insertAdjacentHTML("beforeend", `<li id="episode${episode.id}" class="episodeList-item"><h4 episodeUrl="${episode.url}" class="text">#${episode.id}: ${episode.name}</h4></li>`);
+            const clickEpisodes = document.getElementById(`episode${episode.id}`);
             clickEpisodes.addEventListener("click", printEpisodeInfo);
+        });
+        const btnLoadDropdown = document.getElementById("btnLoadDropdown");
+        btnLoadDropdown.addEventListener("click", () => {
+            asideEpisodes.classList.toggle("episodeList");
         });
         if (JSONdata.info.next) {
             btnLoad.addEventListener("click", () => {
@@ -38,17 +42,28 @@ function printEpisodeInfo(clickEvent) {
         const urlInfoEpisodes = target.getAttribute("episodeUrl");
         const data2 = yield fetch(urlInfoEpisodes);
         const episode = yield data2.json();
-        const displayInfo = `<p class="text">Name: ${episode.name}</p>
+        const displayInfo = `<div d="infoCard" class="infoCard">
+  <h1 class="episodeTitle text">${episode.name}</h1>
   <p class="text">Air Date: ${episode.air_date}</p>
-  <p class="text">Episode: ${episode.episode}</p>`;
+  <p class="text">Episode: ${episode.episode}</p></div>`;
         const printEpisodeContainer = document.getElementById("mainContainer");
         printEpisodeContainer.innerHTML = displayInfo;
         const characters = episode.characters;
         characters.forEach((characters) => __awaiter(this, void 0, void 0, function* () {
             const data3 = yield fetch(characters);
             const characterInfo = yield data3.json();
-            const displayImageCharacter = `<img src=${characterInfo.image} class="characterAvatar">`;
-            console.log(characterInfo.image);
+            const displayImageCharacter = `<div class="characterCards-container">
+      <img
+        src=${characterInfo.image}
+        class="characterCards-avatar"
+      />
+      <div class="characterCards-info">
+        <h3 class="text nameText">${characterInfo.name}</h3>
+        <p class="text">Status: ${characterInfo.status}</p>
+        <p class="text">Species: ${characterInfo.species}</p>
+        <p class="text">Gender: ${characterInfo.gender}</p>
+      </div>
+    </div>`;
             printEpisodeContainer.insertAdjacentHTML("beforeend", displayImageCharacter);
         }));
     });
--- src/script.ts
+++ src/script.ts
@@ -4,9 +4,12 @@ const urlRickMorty = "https://rickandmortyapi.com/api/episode";
 const episodeContainer = document.getElementById(
   "listContainer"
 ) as HTMLUListElement;
+const asideEpisodes = document.getElementById(
+  "episodeContainer"
+) as HTMLDivElement;
 const btnLoad = document.getElementById("btnLoad") as HTMLButtonElement;
 
-//CÓDIGO MARÍA
+//FUNCTION TO PRINT ALL THE EPISODE TITLES
 
 printContent(urlRickMorty);
 
@@ -18,12 +21,19 @@ async function printContent(url: string): Promise<void> {
   episodes.forEach((episode) => {
     episodeContainer.insertAdjacentHTML(
       "beforeend",
-      `<li id="episode${episode.episode}" class="episodeList-item"><h4 episodeUrl="${episode.url}" class="text">${episode.episode}: ${episode.name}</h4></li>`
+      `<li id="episode${episode.id}" class="episodeList-item"><h4 episodeUrl="${episode.url}" class="text">#${episode.id}: ${episode.name}</h4></li>`
     );
     const clickEpisodes = document.getElementById(
-      `episode${episode.episode}`
+      `episode${episode.id}`
     ) as HTMLLIElement;
     clickEpisodes.addEventListener("click", printEpisodeInfo);
+  });
+  const btnLoadDropdown = document.getElementById(
+    "btnLoadDropdown"
+  ) as HTMLButtonElement;
+
+  btnLoadDropdown.addEventListener("click", () => {
+    asideEpisodes.classList.toggle("episodeList");
   });
 
   if (JSONdata.info.next) {
@@ -46,142 +56,34 @@ async function printEpisodeInfo(clickEvent: MouseEvent) {
   const urlInfoEpisodes = target.getAttribute("episodeUrl")!; // ! significa va a existis sí o sí, siempre
   const data2 = await fetch(urlInfoEpisodes);
   const episode: Episode = await data2.json();
-  const displayInfo = `<p class="text">Name: ${episode.name}</p>
+  const displayInfo = `<div d="infoCard" class="infoCard">
+  <h1 class="episodeTitle text">${episode.name}</h1>
   <p class="text">Air Date: ${episode.air_date}</p>
-  <p class="text">Episode: ${episode.episode}</p>`;
+  <p class="text">Episode: ${episode.episode}</p></div>`;
   const printEpisodeContainer = document.getElementById(
     "mainContainer"
   ) as HTMLDivElement;
   printEpisodeContainer.innerHTML = displayInfo;
+
   const characters = episode.characters;
   characters.forEach(async (characters) => {
     const data3 = await fetch(characters);
     const characterInfo: Character = await data3.json();
-    const displayImageCharacter = `<img src=${characterInfo.image} class="characterAvatar">`;
-    console.log(characterInfo.image);
+    const displayImageCharacter = `<div class="characterCards-container">
+      <img
+        src=${characterInfo.image}
+        class="characterCards-avatar"
+      />
+      <div class="characterCards-info">
+        <h3 class="text nameText">${characterInfo.name}</h3>
+        <p class="text">Status: ${characterInfo.status}</p>
+        <p class="text">Species: ${characterInfo.species}</p>
+        <p class="text">Gender: ${characterInfo.gender}</p>
+      </div>
+    </div>`;
     printEpisodeContainer.insertAdjacentHTML(
       "beforeend",
       displayImageCharacter
     );
   });
 }
-
-// async function getTitlesEpisodes(): Promise<Main> {
-//   try {
-//     const titleEpisode = await fetch(urlRM);
-//     const data: Main = await titleEpisode.json();
-//     const episodes: Episode[] = data.results;
-
-//     episodes.forEach((episode) => {
-//       // console.log(episode);
-//       const episodeContainer = document.getElementById(
-//         "listContainer"
-//       ) as HTMLUListElement;
-//       const liEpisode = document.createElement("li");
-//       const h4text = document.createElement("h4");
-
-//       episodeContainer.appendChild(liEpisode);
-//       liEpisode.classList.add("episodeList-item");
-//       liEpisode.appendChild(h4text);
-//       h4text.textContent = episode.name;
-//       h4text.classList.add("text");
-//     });
-
-//     return data;
-//   } catch (error) {
-//     throw new Error("fail");
-//   }
-// }
-
-// getTitlesEpisodes()
-//   .then((dataResults) => {
-//     console.log(dataResults);
-//     getMoreTitles(dataResults);
-//   })
-//   .then((dataResults) => {
-//     console.log(dataResults);
-//   });
-
-// function getMoreTitles(dataResults: Main): void {
-//   const btnLoadEpisodes = document.getElementById(
-//     "btnLoad"
-//   ) as HTMLButtonElement;
-//   let isAlreadyLoad: boolean = true;
-//   btnLoadEpisodes.addEventListener("click", () => {
-//     if (isAlreadyLoad) {
-//       loadMoreTitles(dataResults);
-//       isAlreadyLoad = false;
-//     }
-//   });
-// }
-
-// async function loadMoreTitles(dataResults: Main): Promise<Main> {
-//   try {
-//     const response = await fetch(dataResults?.info.next);
-//     const data: Main = await response.json();
-//     const episodes: Episode[] = data.results;
-
-//     episodes.forEach((episode) => {
-//       const episodeContainer = document.getElementById(
-//         "listContainer"
-//       ) as HTMLUListElement;
-//       // const liEpisode = document.createElement("li");
-//       // liEpisode.textContent = episode.name;
-//       // episodeContainer.appendChild(liEpisode);
-//       const liEpisode = document.createElement("li");
-//       const h4text = document.createElement("h4");
-
-//       episodeContainer.appendChild(liEpisode);
-//       liEpisode.classList.add("episodeList-item");
-//       liEpisode.appendChild(h4text);
-//       h4text.textContent = episode.name;
-//       h4text.classList.add("text");
-//     });
-//     return data;
-//   } catch (error) {
-//     throw new Error("fallo");
-//   }
-// }
-
-// fetch(urlRM)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     const nameEpisode: Episodes[] = data.results;
-//     nameEpisode.forEach((episode: Episodes) => {
-//       console.log(episode.name);
-//       const titleEpisode = episode.name;
-//       episodeContainer.innerHTML += `<li class="episodeList-item">
-//           <h4 class="text">${titleEpisode}</h4>
-//         </li>`;
-//     });
-//   });
-
-// {
-//   data.forEach((element: any) => {
-//     const htmlPosts = ` <a
-//       href="#"
-//       class="list-group-item py-3 lh-sm bg-rm02"
-//       aria-current="true"
-//     >
-//       <div class="d-flex w-100 align-items-center justify-content-between">
-//         <strong class="mb-1">${element}</strong>
-//       </div>
-//       <div class="col-10 mb-1 small">
-//         Some placeholder content in a paragraph below the heading and date.
-//       </div>
-//     </a>`;
-//     episodesDiv.innerHTML += htmlPosts;
-//     console.log(element);
-//   });
-// });
-
-//console.log(data.results.episode));
-
-// fetch(urlUsers)
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-
-// fetch(urlComments)
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
